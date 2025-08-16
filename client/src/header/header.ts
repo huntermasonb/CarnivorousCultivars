bc054e9b65033dd087091363f168e2aee727a87a
Hunter Mason <huntermason15@gmail.com>
2025-08-16 14:59:26 -0400

    MVP Header

--- client/src/header/header.ts
+++ client/src/header/header.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener} from '@angular/core';
 import {RouterLink} from '@angular/router';
 
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {Drawer} from 'primeng/drawer';
+import {ButtonDirective} from 'primeng/button';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
+    Drawer,
+    ButtonDirective,
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class HeaderComponent {
-  closeDropdown(dropdown: any) {
-    dropdown.hide();
+  isMenuOpen = false;
+  constructor(private elementRef: ElementRef){}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    const headerElement = this.elementRef.nativeElement;
+
+    if (!headerElement.contains(target)) {
+      this.closeMenu();
+    }
+  }
+  @HostListener('document:keydown.escape')
+  onEscapeKey(event: Event){
+    this.closeMenu();
   }
 }
